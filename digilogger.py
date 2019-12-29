98ddaaad8e0469a3480bab1b77b6e56b5c38caf5
DigiDuncan <digiduncan@gmail.com>
2019-12-29 03:25:57 -0500

    Initial push.

---
+++ digilogger.py
@@ -0,0 +1,68 @@
+import utils
+digiformatter as df
+
+logChannel = None
+
+
+# Async log functions (prints to console, and logChannel if set)
+async def trace(msg):
+    await log("trace", msg)
+
+
+async def debug(msg):
+    await log("debug", msg)
+
+
+async def info(msg):
+    await log("info", msg)
+
+
+async def warn(msg):
+    await log("warn", msg)
+
+
+async def error(msg):
+    await log("error", msg)
+
+
+async def log(level, msg):
+    msg = str(msg)
+    print(df.formatLog(level, msg))
+    discordPrefix = "```\n"
+    discordSuffix = "\n```"
+    if logChannel is not None:
+        msgMaxLen = 2000 - len(discordPrefix) - len(discordSuffix)
+        for msgPart in utils.chunkStr(msgMaxLen, msg):
+            discordMessage = discordPrefix + msgPart + discordSuffix
+            await logChannel.send(discordMessage)
+
+
+# Sync log functions (prints to console)
+def synctrace(msg):
+    synclog("trace", msg)
+
+
+def syncdebug(msg):
+    synclog("debug", msg)
+
+
+def syncinfo(msg):
+    synclog("info", msg)
+
+
+def syncwarn(msg):
+    synclog("warn", msg)
+
+
+def syncerror(msg):
+    synclog("error", msg)
+
+
+def synclog(level, msg):
+    msg = str(msg)
+    print(df.formatLog(level, msg))
+
+
+def init(channel):
+    global logChannel
+    logChannel = channel
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = []
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="SizeBot",
+    version="3.5.0",
+    author="DigiDuncan",
+    author_email="digiduncan@gmail.com",
+    description="SizeBot3, Cogs Edition, rewritten.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sizedev/SizeBot3AndAHalf",
+    python_requires=">=3.7",
+    install_requires=requirements,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ]
+)
